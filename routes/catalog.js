e35b190c59dfc54c05ad610031aeccdb9e55d0e4
Madhav Malhotra <madhav.89@zoho.com>
2019-10-29 10:52:49 -0400

    Changing controllers

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 // Require controller modules.
-var book_controller = require('../controllers/bookController');
-var author_controller = require('../controllers/authorController');
-var genre_controller = require('../controllers/genreController');
-var book_instance_controller = require('../controllers/bookinstanceController');
+var book_controller = require('../controllers/BookController');
+var author_controller = require('../controllers/AuthorController');
+var genre_controller = require('../controllers/GenreController');
+var book_instance_controller = require('../controllers/BookInstanceController');
 
 /// BOOK ROUTES ///
 
